f75f2218081eb614b7b0fc44a2f49520a6cc105b
Nathan <nathan.prudhomme15@gmail.com>
2026-03-11 14:29:07 +0100

    Fix: BrowserRouter basename pour GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Creations from './pages/Creations';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/nos-creations" element={<Creations />} />
